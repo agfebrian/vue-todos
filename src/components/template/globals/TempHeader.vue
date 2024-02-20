4176bceae3e8f380bde83de8ceea2bd2a8ec90f6
agfebrian <febriananggi9@gmail.com>
2024-02-20 10:39:46 +0700

    init

---
+++ src/components/template/globals/TempHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+    <h1 class="text-2xl font-bold text-gray-950">Vue Todos</h1>
+  </header>
+</template>
---
+++ src/components/template/todos/TempTodos.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="rounded-app bg-white p-6">
+    <h1>Todos</h1>
+  </div>
+</template>
