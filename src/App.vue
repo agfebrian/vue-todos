d061a2764cbb9228609e4fc74bcfe48e8dbf443f
agfebrian <febriananggi9@gmail.com>
2024-02-21 07:52:16 +0700

    feat: fetch current weather

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
-import { ref, provide } from 'vue'
+import { ref, provide, onMounted } from 'vue'
 
 import TempHeader from '@/components/template/globals/TempHeader.vue'
+import TempFooter from '@/components/template/globals/TempFooter.vue'
 import MainTodos from '@/components/template/todos/TempMainTodos.vue'
 import AppContainer from '@/components/app/AppContainer.vue'
 import AppButton from '@/components/app/AppButton.vue'
 
 const toggleSearch = ref<boolean>(false)
 provide('toggleSearch', { toggleSearch })
+
+const weather = ref<string>('')
+const fetchCurrentWeather = async () => {
+  const res = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=-6.338245334410893&longitude=106.92688183226457&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`
+  )
+  const data = await res.json()
+  weather.value = `${data.current.temperature_2m}${data.current_units.temperature_2m} Bekasi`
+}
+
+onMounted(async () => fetchCurrentWeather())
 </script>
 
 <template>
@@ -19,5 +31,8 @@ provide('toggleSearch', { toggleSearch })
       </AppButton>
     </TempHeader>
     <MainTodos />
+    <TempFooter>
+      <p class="text-sm font-medium text-gray-500">Made with ❤ {{ weather }}</p>
+    </TempFooter>
   </AppContainer>
 </template>
---
+++ src/components/template/globals/TempFooter.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="flex flex-col items-center gap-3 py-4">
+    <slot />
+  </footer>
+</template>
